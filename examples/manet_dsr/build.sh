c51d767f14ec076da1f4f791eeffcc1d3f57a434
sanjeev singh <spsingh2@ncsu.edu>
2014-04-23 19:07:28 -0400

    Added inet paths

--- examples/manet_dsr/build.sh
+++ examples/manet_dsr/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-opp_makemake -f -o manet_dsr -O out -L${VEINSHOME}/out/gcc-debug/src/base -L${VEINSHOME}/out/gcc-debug/src/modules -lmiximbase -lmiximmodules
+opp_makemake -f -o manet_dsr -O out -L${VEINSHOME}/out/gcc-debug/src/base -L${VEINSHOME}/out/gcc-debug/src/modules -lmiximbase -lmiximmodules  -L${INETHOME}/out/gcc-debug/src -linet
 make
